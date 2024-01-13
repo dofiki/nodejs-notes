b4f463310a1c491d463f5485c8b35c8e8ff61f8d
Dixit Regmi <dixitregmiofficial@gmail.com>
2024-01-13 17:17:07 +0545

    added all files

---
+++ app.js
@@ -0,0 +1,5 @@
+//using commonJS
+const {add, subtract} = require('./math');
+
+console.log(add(5,5))
+console.log(subtract(10,5))
---
+++ math.js
@@ -0,0 +1,9 @@
+//using commonJs
+
+module.exports.add = function(a, b) {
+    return a + b;
+}
+
+module.exports.subtract = function(a, b) {
+    return a - b;
+}
